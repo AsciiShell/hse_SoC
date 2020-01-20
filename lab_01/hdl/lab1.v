0777c4ae77f36a28a768ba49b79ca48b67480025
asciishell <asciishell@yandex.ru>
2020-01-20 20:41:37 +0300

    Fix circuit

--- lab_01/hdl/lab1.v
+++ lab_01/hdl/lab1.v
@@ -5,10 +5,10 @@ module lab1
     output LED 
 );
 
-    wire a = ~ SW [0];
-    wire b = ~ SW [1];
-    wire c = ~ SW [2];
-	 wire d = ~ SW [3];
+    wire a = SW [0];
+    wire b = SW [1];
+    wire c = SW [2];
+	 wire d = SW [3];
 
 	 assign LED = ~ ((~ (a | b)) | (a & c)) | ((~a) & (~b) & (~c) & (~d));
     
