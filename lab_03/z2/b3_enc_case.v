3a837cce5bbc7689b8928a9e440b312aaa27b0c0
asciishell <asciishell@yandex.ru>
2020-02-09 21:37:19 +0300

    Add encoder

--- lab_03/z2/b3_enc_case.v
+++ lab_03/z2/b3_enc_case.v
@@ -2,7 +2,7 @@ module b3_enc_case(
    input [15:0] encoder_in,
    output reg [3:0] binary_out,
    input enable
-  );
+);
 
 
   always @ (*)
@@ -20,12 +20,12 @@ module b3_enc_case(
 			16'h80  : binary_out = 7;
 			16'h100 : binary_out = 8;
 			16'h200 : binary_out = 9;
-                        16'h400  : binary_out = 10;
-			16'h800  : binary_out = 11;
-			16'h1000  : binary_out = 12;
+            16'h400 : binary_out = 10;
+			16'h800 : binary_out = 11;
+			16'h1000 : binary_out = 12;
 			16'h2000 : binary_out = 13;
 			16'h4000 : binary_out = 14;
-                        16'h8000 : binary_out = 15;
+            16'h8000 : binary_out = 15;
 		 endcase
     end
   end
--- lab_03/z2/b4_pri_enc_if.v
+++ lab_03/z2/b4_pri_enc_if.v
@@ -8,38 +8,22 @@ always @ (*)
 begin
   binary_out = 0;
   if (enable) begin
-   if (encoder_in[0] == 1'b1)
-		binary_out = 0;
-    else if (encoder_in[1] == 1'b1)
-		binary_out = 1;
-    else if (encoder_in[2] == 1'b1)
-		binary_out = 2;
-    else if (encoder_in[3] == 1'b1)
-		binary_out = 3;
-    else if (encoder_in[4] == 1'b1)
-		binary_out = 4;
-    else if (encoder_in[5] == 1'b1)
-		binary_out = 5;
-    else if (encoder_in[6] == 1'b1)
-		binary_out = 6;
-    else if (encoder_in[7] == 1'b1)
-		binary_out = 7;
-    else if (encoder_in[8] == 1'b1)
-		binary_out = 8;
-    else if (encoder_in[9] == 1'b1)
-		binary_out = 9;
-    else if (encoder_in[10] == 1'b1)
-		binary_out = 10;
-    else if (encoder_in[11] == 1'b1)
-		binary_out = 11;
-    else if (encoder_in[12] == 1'b1)
-		binary_out = 12;
-    else if (encoder_in[13] == 1'b1)
-		binary_out = 13;
-    else if (encoder_in[14] == 1'b1)
-		binary_out = 14;
-    else if (encoder_in[15] == 1'b1)
-		binary_out = 15;
+   if (encoder_in[0] == 1'b1) binary_out =  0;
+    else if (encoder_in[1] == 1'b1) binary_out =  1;
+    else if (encoder_in[2] == 1'b1) binary_out =  2;
+    else if (encoder_in[3] == 1'b1) binary_out =  3;
+    else if (encoder_in[4] == 1'b1) binary_out =  4;
+    else if (encoder_in[5] == 1'b1) binary_out =  5;
+    else if (encoder_in[6] == 1'b1) binary_out =  6;
+    else if (encoder_in[7] == 1'b1) binary_out =  7;
+    else if (encoder_in[8] == 1'b1) binary_out =  8;
+    else if (encoder_in[9] == 1'b1) binary_out =  9;
+    else if (encoder_in[10] == 1'b1) binary_out =  10;
+    else if (encoder_in[11] == 1'b1) binary_out =  11;
+    else if (encoder_in[12] == 1'b1) binary_out =  12;
+    else if (encoder_in[13] == 1'b1) binary_out =  13;
+    else if (encoder_in[14] == 1'b1) binary_out =  14;
+    else if (encoder_in[15] == 1'b1) binary_out =  15;
 
   end
 end
