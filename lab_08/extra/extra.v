64a3b210e0439b7b186326c3721b14b9b03e4d11
asciishell <asciishell@yandex.ru>
2020-03-22 21:50:55 +0300

    Extra 1

--- lab_08/extra/extra.v
+++ lab_08/extra/extra.v
@@ -1,18 +1,17 @@
 module extra
 (
-input clock,
-input reset_n,
-input enable,
-input a0,
-input a1,
-input a2,
-input a3,
-output reg [3:0] y
+	input clock,
+	input reset_n,
+	input enable,
+	input [1:0] a,
+	output reg [1:0] y
 );
 
 parameter [1:0] S0 = 0, S1 = 1, S2 = 2, S3 = 3;
+parameter [1:0] A0 = 0, A1 = 1, A2 = 2, A3 = 3;
+parameter [1:0] Y0 = 0, Y1 = 1, Y2 = 2, Y3 = 3;
 
-reg [1:0] state, next_state;
+reg [1:0] state, next_state, next_out;
 
 // State register
 
@@ -27,42 +26,105 @@ always @ (posedge clock or negedge reset_n)
 always @*
 	case (state)
 	S0:
-		if (a0 | a3)
-			next_state = S3;
-		else
-			next_state = S2;
-	S1:
-		if (a0 | a2)
-			next_state = S2;
-		else if (a1)
-			next_state = S3;
-		else
-			next_state = S1;
-	S2:
-		if (a0 | a3)
-			next_state = S1;
-		else if (a1)
+		case(a)
+		A0: begin
 			next_state = S0;
-
-		else
-			next_state = S2;
-	S3:
-		if (a0 | a1)
+			next_out = Y0;
+		end
+		A1: begin
 			next_state = S1;
-		else if (a2 | a3)
+			next_out = Y0;
+		end
+		A2: begin
+			next_state = S0;
+			next_out = Y1;
+		end
+		A3: begin
 			next_state = S2;
-	default:
+			next_out = Y3;
+		end
+		default: begin
+			next_state = S0;
+			next_out = Y0;
+			end
+		endcase
+	S1:
+		case(a)
+		A0: begin
+			next_state = S1;
+			next_out = Y1;
+		end
+		A1: begin
+			next_state = S1;
+			next_out = Y2;
+		end
+		A2: begin
+			next_state = S1;
+			next_out = Y3;
+		end
+		A3: begin
+			next_state = S2;
+			next_out = Y1;
+		end
+		default: begin
+			next_state = S0;
+			next_out = Y0;
+		end
+		endcase
+	S2:
+		case(a)
+		A0: begin
+			next_state = S0;
+			next_out = Y1;
+		end
+		A1: begin
+			next_state = S2;
+			next_out = Y2;
+		end 
+		A2: begin
+			next_state = S0;
+			next_out = Y0;
+		end
+		A3: begin
+			next_state = S3;
+			next_out = Y0;
+		end 
+		default: begin
+			next_state = S0;
+			next_out = Y0;
+		end
+		endcase
+	S3:
+		case(a)
+		A0: begin
+			next_state = S2;
+			next_out = Y1;
+		end
+		A1: begin
+			next_state = S0;
+			next_out = Y2;
+		end 
+		A2: begin
+			next_state = S0;
+			next_out = Y3;
+		end
+		A3: begin
+			next_state = S3;
+			next_out = Y1;
+		end
+		default: begin
+			next_state = S0;
+			next_out = Y0;
+		end
+		endcase
+	default: begin
 		next_state = S0;
-
+		next_out = Y0;
+		end
 endcase
 
 always @ (posedge clock)
-	case (state)
-		S0: y[0] <= 1;
-		S1: y[1] <= 1;
-		S2: y[2] <= 1;
-		S3: y[3] <= 1;
-		default:
-			y <= 4'b0000;
-	endcase
+begin
+	y <= next_out;
+end
 endmodule
